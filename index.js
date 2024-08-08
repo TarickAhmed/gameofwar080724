c1361c81fea7641a65aea0cc0ab38e0f6772ee2f
Tarick Ahmed <newstartin702@gmail.com>
2024-08-07 20:44:04 -0700

    Game of war commit

---
+++ index.js
@@ -0,0 +1,47 @@
+class deck{
+constructor(){
+this.deck = [];
+this.ranks=[]
+"Ace"
+}"2"
+"3"
+"4"
+"5"
+}"6"
+"7"
+"8"
+"9"
+"10"
+"Jack"
+"Queen"
+"King"
+this.suits=[]
+"Hearts";"Diamonds";"Spades;"Clubs";{
+
+
+createDeck(){
+
+}
+}for (let i=0,i< this suits.length;i++)
+for (let j=0, j=this.ranks.length;j++)
+name:`$(This.ranks`(j) of $(this.suits[i])
+}
+    this.deck.push;{
+value:j+1 
+
+this.deck.push(card)
+
+console.log(card)
+
+
+
+
+    cons deck= new Deck
+)console.log(deck)
+deck.createDeck()
+console.log
+
+    }
+
+
+}
